9b9d98dd2e0c4721e255e67738e73a7e44b9b1f9
andrephelipe <andre.phelipe.1999@gmail.com>
2023-05-17 18:58:02 -0300

    instalando o lint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-const { readPlanets, writeNewPlanet } = require('./utils/fsUtils');
+const readline = require('readline-sync');
+const { writeNewPlanet } = require('./utils/fsUtils');
 
 const main = async () => {
-  writeNewPlanet({
-      "id": 4,
-      "nome": "Plutao",
-      "habitantes": 19,
-      "corDoCeu": "verde"
-  })
+  // writeNewPlanet({
+  //     "id": 4,
+  //     "nome": "Plutao",
+  //     "habitantes": 19,
+  //     "corDoCeu": "verde"
+  // })
+
+  const namePlanet = readline.question('Qual o nome do planeta? ');
+  const habitantes = readline.questionInt('Quantos habitantes ha nesse planeta? ');
+  const corDoCeu = readline.question('Qual a cor do ceu? ');
+
+  const newPlanet = { namePlanet, habitantes, corDoCeu };
+  writeNewPlanet(newPlanet);
+  console.log('Novo planeta cadastrado com sucesso!');
 };
 
 main();
--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -1,30 +1,33 @@
 const fs = require('fs').promises;
 const path = require('path');
-const PLANETS_DATA_PATH = '../../data/planets.json'
+
+const PLANETS_DATA_PATH = '../../data/planets.json';
 
 const readPlanets = async () => {
   try {
     const data = await fs.readFile(path.resolve(__dirname, PLANETS_DATA_PATH));
-    const planets = JSON.parse(data)
-    return planets
+    const planets = JSON.parse(data);
+    return planets;
   } catch (error) {
-    console.error(`Erro na leitura do arquivo: ${error}`)
+    console.error(`Erro na leitura do arquivo: ${error}`);
   }
 };
 
 const writeNewPlanet = async (newPlanets) => {
   try {
-    const oldPlanets = await readPlanets()
-    const allPlanets = JSON.stringify([...oldPlanets, newPlanets])
+    const oldPlanets = await readPlanets();
+    const allPlanets = JSON.stringify([
+      ...oldPlanets, 
+      { id: Date.now(), ...newPlanets },
+    ]);
 
-    await fs.writeFile(path.resolve(__dirname, PLANETS_DATA_PATH), allPlanets)
-
+    await fs.writeFile(path.resolve(__dirname, PLANETS_DATA_PATH), allPlanets);
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 };
 
 module.exports = {
   readPlanets,
-  writeNewPlanet
+  writeNewPlanet,
 };
